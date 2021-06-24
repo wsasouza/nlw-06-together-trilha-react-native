70ddf6d3300ea50b69e3699cadd15ce99e5c32d3
wsasouza <wsasouza@hotmail.com>
2021-06-23 22:03:14 -0300

    Create header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
@@ -10,7 +11,7 @@ export function Home() {
     <View>
       <View style={styles.header}>
         <Profile />
-
+        <ButtonAdd />
       </View>
     </View>
   )
